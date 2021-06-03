a778e30aae0558cd47a64223fe90dbbd21db73db
tongliang <852481695@qq.com>
2021-06-03 19:03:09 +0800

    Initial commit

---
+++ src/com/bjsxt/interceptor/MyInterceptor.java
@@ -0,0 +1,25 @@
+package com.bjsxt.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("PreHandle: 在处理请求的目标方法执行之前");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("PostHandle: 在目标方法执行之后，视图解析器处理视图之前");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion: 为客户端产生响应之前");
+    }
+}
